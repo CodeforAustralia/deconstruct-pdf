415dc507a5622f09cbc0e280bb65a6c05b1bb44b
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-10-11 05:50:07 +1100

    Add insert into letter_history

--- populate-db.sh
+++ populate-db.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-while getopts d:l:r:t:u: option
+while getopts d:l:p:r:t:u: option
 do
  case "${option}"
  in
  d) TARGET_DB=${OPTARG};;
  l) LETTER_DATE=${OPTARG};;
+ p) NO_OF_PAGES==${OPTARG};;
  r) REF_NO=${OPTARG};;
  t) TEMPLATE_NO=${OPTARG};;
  u) UUID=$OPTARG;;
@@ -18,9 +19,16 @@ done
 #echo "Service ID = $REF_NO"
 #echo "Template = $TEMPLATE_NO"
 #echo "UUID = $UUID"
- 
+#echo "NO_OF_PAGES = $NO_OF_PAGES" 
+
 mysql -uroot $TARGET_DB <<EOF
+SET autocommit=0;
+START TRANSACTION;
 INSERT INTO letters (uuid, reference_id, template_id, filename, letter_date, created_at, updated_at) VALUES 
 ('$UUID', '$REF_NO', '$TEMPLATE_NO', '$UUID.html', '$LETTER_DATE', now(), now());
-commit;
+
+INSERT INTO letter_history (reference_id, user_id, letter_uuid, created_at, updated_at) 
+(select user_services.reference_id, user_services.user_id, '$UUID' as letter_uuid, now(), now() from user_services where reference_id='$REF_NO');
+COMMIT;
+SET autocommit=1;
 EOF
